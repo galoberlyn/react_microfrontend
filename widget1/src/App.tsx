f69f5607edac328f97a71b548f4a54c4e1951fb6
galoberlyn <gbgarlejo@gmail.com>
2023-11-28 23:11:58 +0800

    Add css support

--- widget1/src/App.tsx
+++ widget1/src/App.tsx
@@ -1,8 +1,10 @@
+import './assets/css/app.style.css';
+
 function App() {
 
   return (
     <>
-      <button>Iam widget 1, hi!</button>
+      <button className="widget1">Iam widget 1, hi!</button>
     </>
   )
 }
--- widget1/vite.config.ts
+++ widget1/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   server: {
     port: 3001
   },
